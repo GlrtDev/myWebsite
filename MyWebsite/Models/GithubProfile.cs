ee810ff406f6eb7208171bdf2435ed844e2218c9
GlrtDev <subin7777777@gmail.com>
2020-02-10 19:20:25 +0100

    Add github profile model

---
+++ MyWebsite/Models/GithubProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace MyWebsite.Models
+{
+    public class GithubProfile
+    {
+        string login { get; set; }
+        string avatar_url { get; set; }
+        string html_url {get; set;}
+        [JsonPropertyName("public_repos")]
+        int repos_number { get; set; }
+    }
+}
