f474c2af37222133678d0e71bb84d4b753cb38cf
GlrtDev <subin7777777@gmail.com>
2020-02-16 21:53:30 +0100

    Add ToString override

--- MyWebsite/Models/GithubProfile.cs
+++ MyWebsite/Models/GithubProfile.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace MyWebsite.Models
 {
     public class GithubProfile
     {
-        string login { get; set; }
-        string avatar_url { get; set; }
-        string html_url {get; set;}
+        public string Login { get; set; }
+        public string Avatar_url { get; set; }
+        public string Html_url {get; set;}
         [JsonPropertyName("public_repos")]
-        int repos_number { get; set; }
+        public int Repos_number { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<GithubProfile>(this);
     }
 }
