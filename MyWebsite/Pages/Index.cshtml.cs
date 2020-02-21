f1f0dcef9f682a70f514ea4ee350eddff95d58d9
GlrtDev <subin7777777@gmail.com>
2020-02-21 23:55:12 +0100

    Add GithubProfileService to Index page model

--- MyWebsite/Pages/Index.cshtml.cs
+++ MyWebsite/Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using MyWebsite.Services;
+using MyWebsite.Models;
 
 namespace MyWebsite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public JsonFileGithubProfileService GithubProfileService;
+        public IEnumerable<GithubProfile> GithubProfiles { get; private set; }
+        
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileGithubProfileService profileService)
         {
             _logger = logger;
+            GithubProfileService = profileService;
         }
 
         public void OnGet()
         {
-
+            GithubProfiles = GithubProfileService.GetGithubProfiles();
         }
     }
 }
